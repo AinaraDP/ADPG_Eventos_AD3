e53be57a0af66b74b936e5d247fa1192c5be3721
Ainara De Pablo <122480987+AinaraDP@users.noreply.github.com>
2024-10-08 17:18:31 +0200

    subida

---
+++ src/main/java/eventos/AdpgAd3EventosApplication.java
@@ -0,0 +1,13 @@
+package eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdpgAd3EventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdpgAd3EventosApplication.class, args);
+	}
+
+}
